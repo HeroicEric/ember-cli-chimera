2ae4109d600e68854b139ef0ad7833d74eb58f78
Eric Kelly <heroiceric@gmail.com>
2015-09-17 11:05:04 -0400

    Add some samples to the initializer

--- addon/instance-initializers/ember-cli-chimera.js
+++ addon/instance-initializers/ember-cli-chimera.js
@@ -4,7 +4,19 @@ export function initialize(instance) {
   const { container } = instance;
   const fakeStore = container.lookup('service:fake-store');
 
+  fakeStore.createRecord('user', {
+    id: 1,
+    firstName: 'Eric',
+    lastName: 'Kelly'
+  });
+
   const server = new Server(fakeStore);
+
+  server.createRoute('get', '/users/:id', function(store, request) {
+    const user = store.peekRecord('user', request.params.id);
+
+    return server.respondWith(200, user);
+  });
 }
 
 export default {
